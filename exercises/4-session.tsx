503e1f2dad325dfb6db0bb3daed886734676a548
Glenn Reyes <glenn@glennreyes.com>
2024-04-04 11:51:37 +0100

    Fix forwardRef

--- exercises/4-session.tsx
+++ exercises/4-session.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import type { FC, ForwardedRef } from 'react';
+
 import { selectClasses } from '@/components/ui/select';
 import { cn } from '@/lib/utils';
-import { type FC, forwardRef, useRef } from 'react';
+import { forwardRef, useRef } from 'react';
 
 const currencies = {
   chf: 'CHF',
@@ -48,14 +50,34 @@ function Select<TMulti extends boolean = false>({
   );
 }
 
+// interface ParagraphProps extends ComponentProps<'p'> {}
+
+// const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(function Paragraph(
+//   { children }: { children: React.ReactNode },
+//   ref: React.Ref<HTMLParagraphElement>,
+// ) {
+//   return <p ref={ref}>{children}</p>;
+// }
+
+declare module 'react' {
+  function forwardRef<T, P>(
+    render: (props: P, ref: Ref<T>) => ReactNode | null,
+  ): (props: P & RefAttributes<T>) => ReactNode | null;
+}
+
 interface GenericSelectProps<TMulti> {
   children: React.ReactNode;
   isMulti?: TMulti;
 }
 
-const GenericSelect = forwardRef(function Select<
+const GenericSelect = forwardRef(function Select123<
   TMulti extends boolean = false,
->({ children, isMulti }: GenericSelectProps<TMulti>) {
+>(
+  { children, isMulti }: GenericSelectProps<TMulti>,
+  ref: ForwardedRef<HTMLSelectElement>,
+) {
+  console.log(ref);
+
   return (
     <select className={selectClasses} multiple={isMulti}>
       {children}
